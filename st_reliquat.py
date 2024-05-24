0e4f1205232063db798865f8340c2e8748d9584a
Tkarsk <thomas.karskens@bpost.be>
2024-05-24 16:15:47 +0200

    second commit

--- st_reliquat.py
+++ st_reliquat.py
@@ -8,17 +8,14 @@ import folium
 import altair as alt
 from streamlit_echarts import st_echarts
 
-
 st.set_page_config(layout="wide")
 
 st.sidebar.title("Navigation")
-option = st.sidebar.selectbox("Sélectionner un tableau:", ["Nombre de reliquat par CTI", "Raisons des reliquats par CTI","Aperçu des données"])
+option = st.sidebar.selectbox("Sélectionner un tableau:", ["Nombre de reliquat par CTI", "Raisons des reliquats par CTI", "Aperçu des données"])
 
 st.title("Reliquat - Analysis")
 
 uploaded_file = st.sidebar.file_uploader("Télécharger le fichier .csv reliquat", type=["csv"])
-
-
 
 @st.cache_data
 def load_and_process_data(file_content, start_date, end_date):
@@ -84,11 +81,6 @@ if uploaded_file is not None:
         st.header("Nombre de reliquat par CTI")
         st_echarts(options=options_cti, height="700px")
 
-
-    #######
-
-
-
         df_grouped = df_cleaned.groupby('name_mail_center')[['reliquat_letters', 'reliquat_parcels', 'reliquat_ZZA_ENA', 'reliquat_restmail', 'reliquat_parcelsretours']].sum().reset_index()
         rows_to_exclude = ["1099 - NEW BRUSSEL X", "2099 - NEW ANTWERPEN X", "4099 - LIEGE X", "6099 - CHARLEROI X", "9099 - GENT X"]
         df_grouped = df_grouped[~df_grouped['name_mail_center'].isin(rows_to_exclude)]
@@ -108,13 +100,6 @@ if uploaded_file is not None:
         - **reliquat_parcelsretours**: Nombre de retours de colis en reliquat
         """)
 
-
-
-    #######
-
-
-    # Affiche le CTI et le type de relicat au cours de 2023 / 2024
-
         df_cleaned['date'] = pd.to_datetime(df_cleaned['date'], format='%d/%m/%Y')
         grouped_data_time_cti = df_cleaned.groupby(['date', 'transport_center']).sum().reset_index()
 
@@ -130,17 +115,13 @@ if uploaded_file is not None:
 
         st.subheader(f"Évolution du {selected_reliquat} pour le CTI : {selected_cti}")
 
-
         st.line_chart(df_cti.set_index('date')[selected_reliquat], height=300, width=700, use_container_width=True)
 
-
-
-    #############################################
-
-    # RAISON RELIQUAT
-
-    # Raisons des reliquats par CTI
     elif option == "Raisons des reliquats par CTI":
+        df_cleaned = pd.read_csv(uploaded_file, encoding='cp1252', sep=',', quotechar='"', on_bad_lines="skip")
+        df_cleaned['date'] = pd.to_datetime(df_cleaned['date'], format='%d/%m/%Y', errors='coerce')
+        df_cleaned = df_cleaned.dropna(subset=['date'])
+        
         df_count = df_cleaned.groupby(['transport_center', 'reason']).size().unstack(fill_value=0)
 
         st.header("Raisons des reliquats par CTI")
@@ -152,9 +133,6 @@ if uploaded_file is not None:
         - **A**: Autre
         """)
 
-
-    #######
-
         df_count = df_cleaned.groupby(['name_mail_center', 'reason']).size().unstack(fill_value=0)
 
         st.header("Raisons des reliquats par mail center")
@@ -165,8 +143,6 @@ if uploaded_file is not None:
         - **T**: Trop tard
         - **A**: Autre
         """)
-
-    #######
 
         st.header("Évolution des raisons des reliquats par CTI dans le temps")
 
@@ -184,19 +160,11 @@ if uploaded_file is not None:
 
         st.line_chart(df_time_reason_grouped_cti.set_index('date')['count'], height=300, width=700, use_container_width=True)
 
-
-
-
-
-    #######
-
-
         st.header("Évolution des raisons des reliquats par mail center dans le temps")
 
         mail_center_list = df_cleaned['name_mail_center'].unique()
         selected_mail_center = st.selectbox("Sélectionnez un mail center pour l'évolution dans le temps", mail_center_list)
         
-        reason_list = df_cleaned['reason'].unique()
         selected_reason = st.selectbox("Sélectionnez la raison du reliquat", reason_list, key="reason_mail_center")
 
         df_time_reason_mail_center = df_cleaned[(df_cleaned['name_mail_center'] == selected_mail_center) & 
@@ -206,11 +174,3 @@ if uploaded_file is not None:
         st.subheader(f"Évolution de la raison '{selected_reason}' pour le mail center : {selected_mail_center}")
 
         st.line_chart(df_time_reason_grouped_mail_center.set_index('date')['count'], height=300, width=700, use_container_width=True)
-
-
-    #############################################
-
-
-
-
-
